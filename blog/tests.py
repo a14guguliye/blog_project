841a3cc5863b5ee8c825d886592c417daa938156
a14guguliye <quluquliyev08@gmail.com>
2021-02-22 01:40:39 +0400

    finalcommit

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+from .models import Post
+from .models import *
 
 # Create your tests here.
+
+class BlogTests(TestCase):
+    def setUp(self):
+        self.user=get_user_model().objects.create(
+            username='testuser',
+            email='testuser@gmail.com',
+            password='secret'
+        )
+
+        self.post=Post.objects.create(
+            title='test_title',
+            body='nice body content',
+            author=self.user
+        )
+    
+    def test_string_representation(self):
+        post=Post(title="A Sample Test")
+        self.assertEquals(str(post),post.title)
+    
+
+    def test_post_content(self):
+        self.assertEquals(self.post.title,'test_title')
+        self.assertEquals(self.post.body,'nice body content')
+        #self.assertEquals(self.post.author,'testuser')
+    
+    def test_post_list(self):
+        response=self.client.get(reverse('home'))
+        self.assertEquals(response.status_code,200)
+        self.assertContains(response,'nice body content')
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_post_detail_view(self):
+        response=self.client.get('/post/1')
+        no_response=self.client.get('/post/2')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(no_response.status_code,404)
+        self.assertContains(response, 'test_title')
+        self.assertTemplateUsed(response,'post_detail.html')
--- readme.txt
+++ readme.txt
@@ -5,3 +5,6 @@ objective
 4) learning how to register app and template in settings.py file 
 5) learning how to work with listView 
 6) passing the data from view to the html file 
+7) learning how to configure the static files 
+8) learning how to work with detailView class 
+9) learning how to test the porject 
