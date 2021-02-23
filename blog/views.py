6633dfbce8d46b6cff5fc1531b97a922faf0e033
a14guguliye <quluquliyev08@gmail.com>
2021-02-24 02:02:21 +0400

    updatesaveviewadded

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from  django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
 
 from .models import *
 # Create your views here.
@@ -13,3 +15,16 @@ class BlogDetailView(DetailView):
     model=Post
     template_name='post_detail.html'
     context_object_name='post'
+
+
+
+class BlogCreateView(CreateView):
+    model=Post
+    template_name='post_new.html'
+    fields='__all__'
+
+
+class BlogUpdateView(UpdateView):
+    model=Post
+    template_name='post_edit.html'
+    fields=['title','body']
